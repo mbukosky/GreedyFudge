630b923921eea309f0394c44bbab4406afc6b892
Curt Layton <laytonc@hotmail.com>
2015-01-30 15:13:21 -0500

    polished our awesome UI

--- public/modules/games/config/games.client.routes.js
+++ public/modules/games/config/games.client.routes.js
@@ -1,1 +1,17 @@
-﻿var foo = "bar";
+﻿'use strict';
+
+// Setting up route
+angular.module('articles').config(['$stateProvider',
+	function ($stateProvider) {
+        // Articles state routing
+        $stateProvider.
+		state('game', {
+            url: '/game',
+            templateUrl: 'modules/games/views/board.client.view.html'
+        }).
+        state('endGame', {
+            url: '/endGame',
+            templateUrl: 'modules/games/views/end.client.view.html'
+        });
+    }
+]);
--- public/modules/games/controllers/games.client.controller.js
+++ public/modules/games/controllers/games.client.controller.js
@@ -1,4 +1,32 @@
-﻿angular.module('articles').controller("GamesController", function ($scope) {
-    $scope.players = ['player1', 'player2'];
-    $scope.questions = ['round 1', 'round2'];
+﻿angular.module('articles').controller("GamesController", function ($scope,$location) {
+    $scope.Score = 0;
+    $scope.Round = 0;
+    //$scope.userResponse = "";
+    $scope.Questions = [{ Definition: 'Abbey Road', Answers: [{ Response: 'Beatles', IsCorrect: true }, { Response: 'Metallica', IsCorrect: false }] }, { Definition: 'Appetite for Destruction', Answers: [{ Response: 'Beatles', IsCorrect: true }, { Response: 'Metallica', IsCorrect: false }] }];
+    
+
+    $scope.SubmitAnswer = function () {
+        //validate answer
+        //if ($scope.userReponse != null) {
+            ValidateResponse();
+            $scope.Round++;
+
+            // move to next round if there is a question. otherwise go to another view and say good job.
+            if ($scope.Round >= $scope.Questions.length) {
+                // how do we pass score?
+                $location.path('endGame');
+            }
+        //}
+    };
+    $scope.NewGame = function () {
+        //reset questions, score, round etc.
+        console.log("new game");
+        
+        $location.path('/game');
+    };
+
+    ValidateResponse = function () {
+        console.log($scope.userResponse);
+        $scope.Score++;
+    };
 });
